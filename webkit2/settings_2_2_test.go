617c894ef66238ef11cc41b64803fcd2d159bd0a
Bryan Summersett <bsummersett@gmail.com>
2013-11-02 00:04:08 -0700

    fixing tests for < 2.2

---
+++ webkit2/settings_2_2_test.go
@@ -0,0 +1,23 @@
+// +build !webkit2_2_1,!webkit2_2_0
+
+package webkit2
+
+import (
+    "testing"
+)
+
+func TestSettings_EnableWriteConsoleMessagesToStdout(t *testing.T) {
+    s := NewWebView().Settings()
+
+    write := s.GetEnableWriteConsoleMessagesToStdout()
+    wantWrite := !write
+    s.SetEnableWriteConsoleMessagesToStdout(wantWrite)
+
+    write = s.GetEnableWriteConsoleMessagesToStdout()
+    if wantWrite != write {
+        t.Errorf("want changed Write == %d, got %d", wantWrite, write)
+    }
+
+    // Revert to original setting.
+    s.SetEnableWriteConsoleMessagesToStdout(!write)
+}
