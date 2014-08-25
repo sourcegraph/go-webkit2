985457ebbcaccd9e3dc4e5eae181a43ba82cb1aa
Mihael Dimec <miha.dimec@visionect.com>
2014-08-25 15:20:07 +0200

    cookie local storage gotk3/go-webkit2
    Conflicts:
    	src/github.com/visionect/go-webkit2/webkit2/webcontext.go
    	src/okular/webkit/main.go

--- webkit2/webcontext.go
+++ webkit2/webcontext.go
@@ -2,13 +2,22 @@ package webkit2
 
 // #include <webkit2/webkit2.h>
 import "C"
-
+import (
+	"unsafe"
+	"github.com/visionect/gotk3/glib"
+)
 // WebContext manages all aspects common to all WebViews.
 //
 // See also: WebKitWebContext at
 // http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebContext.html.
 type WebContext struct {
+	*glib.Object
 	webContext *C.WebKitWebContext
+}
+
+func newWebContext(webContext *C.WebKitWebContext) *WebContext {
+	obj := &glib.Object{glib.ToGObject(unsafe.Pointer(webContext))}
+	return &WebContext{obj, webContext}
 }
 
 // DefaultWebContext returns the default WebContext.
@@ -16,7 +25,7 @@ type WebContext struct {
 // See also: webkit_web_context_get_default at
 // http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebContext.html#webkit-web-context-get-default.
 func DefaultWebContext() *WebContext {
-	return &WebContext{C.webkit_web_context_get_default()}
+	return newWebContext(C.webkit_web_context_get_default())
 }
 
 // CacheModel describes the caching behavior.
@@ -56,3 +65,24 @@ func (wc *WebContext) SetCacheModel(model CacheModel) {
 func (wc *WebContext) ClearCache() {
 	C.webkit_web_context_clear_cache(wc.webContext)
 }
+
+// SetDiskCacheDirectory sets the directory where disk cache files will be stored .
+//
+// See also: webkit_web_context_set_disk_cache_directory
+// http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebContext.html#webkit-web-context-set-disk-cache-directory
+func (wc *WebContext) SetDiskCacheDirectory(directory string) {
+	C.webkit_web_context_set_disk_cache_directory(wc.webContext, (*C.gchar)(C.CString(directory)))
+}
+
+func (wc *WebContext) GetCookieManager() *CookieManager {
+	return newCookieManager(C.webkit_web_context_get_cookie_manager(wc.webContext))
+}
+
+// SetWebExtensionsDirectory sets the directory where WebKit will look for Web Extensions.
+//
+// See also: webkit_web_context_set_web_extensions_directory
+// http://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebContext.html#webkit-web-context-set-web-extensions-directory
+func (wc *WebContext) SetWebExtensionsDirectory(directory string) {
+	C.webkit_web_context_set_web_extensions_directory(wc.webContext, (*C.gchar)(C.CString(directory)))
+}
+
