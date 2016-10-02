9922feec7804911584929d956f5c85422bc03992
Paul Tagliamonte <tag@pault.ag>
2016-10-02 17:10:57 -0400

    Change import path gotk3's new path

--- cmd/webkit-eval-js/evaljs.go
+++ cmd/webkit-eval-js/evaljs.go
@@ -9,8 +9,8 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/conformal/gotk3/glib"
-	"github.com/conformal/gotk3/gtk"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 	"github.com/sourcegraph/go-webkit2/webkit2"
 	"github.com/sqs/gojs"
 )
